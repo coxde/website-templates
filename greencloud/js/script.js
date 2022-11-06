f6ed2aa64e715e84b9fde26d2c4a4166ef5cdc6f
coxde <63153334+coxde@users.noreply.github.com>
2022-11-06 15:36:34 +0800

    Add media queries to be responsive Add mobile navigation

--- greencloud/js/script.js
+++ greencloud/js/script.js
@@ -2,3 +2,24 @@
 const yearEl = document.querySelector('.year')
 const currentYear = new Date().getFullYear()
 yearEl.textContent = currentYear
+
+// Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav')
+const headerEl = document.querySelector('.header')
+
+btnNavEl.addEventListener('click', function () {
+    headerEl.classList.toggle('nav-open')
+})
+
+// Close mobile navigation
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        if (link.classList.contains('nav-list-item')) {
+            if (headerEl.classList.contains('nav-open')) {
+                headerEl.classList.remove('nav-open')
+            }
+        }
+    })
+})
