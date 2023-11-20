b8e80757c519037317ad7b4c47f22d8f5ecd3917
coxde <63153334+coxde@users.noreply.github.com>
2023-11-20 00:29:09 +0000

    Add responsive layout

--- digitalcraft/js/script.js
+++ digitalcraft/js/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Set current year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
 // Sticky navigation when the first section is intersected
 const sectionFirstEl = document.querySelector('main').firstElementChild;
 
@@ -25,7 +30,23 @@ const obs = new IntersectionObserver(
 
 obs.observe(sectionFirstEl);
 
-// Set current year
-const yearEl = document.querySelector('.year');
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+// Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header--container');
+
+btnNavEl.addEventListener('click', function () {
+    headerEl.classList.toggle('nav-open');
+});
+
+// Close mobile navigation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        if (link.classList.contains('nav-list__item')) {
+            if (headerEl.classList.contains('nav-open')) {
+                headerEl.classList.remove('nav-open');
+            }
+        }
+    });
+});
